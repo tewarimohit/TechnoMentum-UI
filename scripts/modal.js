c3dc1dc5f66597b45aad0fcfee1e66cb27c5c5f2
Mohit Tewari <tewarimohit955@gmail.com>
2022-02-09 22:56:55 +0530

    Feat: Documentation of components
    Did the documentation of components uploaded till now.

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,15 +1,15 @@
-const modalBtn=document.querySelector('.modal-btn');
-const modalBg=document.querySelector('.modal-bg');
-const modalClose=document.querySelector('.modal-close');
+const modalBtn = document.querySelector('.modal-btn');
+const modalBg = document.querySelector('.modal-bg');
+const modalClose = document.querySelector('.modal-close');
 
 
-modalBtn.addEventListener('click',function(){
+modalBtn.addEventListener('click', function () {
 
     modalBg.classList.add('bg-active');
 })
 
 
-modalClose.addEventListener('click',function(){
+modalClose.addEventListener('click', function () {
 
     modalBg.classList.remove('bg-active');
 })
